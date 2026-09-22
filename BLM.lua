506e67eb3575afaf4e310b1a3ddd499171c9bc46
Erich Healy <ErichRHealy@gmail.com>
2026-09-21 18:03:44 -0700

    code reorganiztation

--- BLM.lua
+++ BLM.lua
@@ -7,7 +7,7 @@ local CommonSets = require("common_sets");
 local Bursts = require("bursts");
 local events = require("events");
 local ui = require("ui");
-local SetBuilder = require('setbuilder')
+local SetBuilder = require("setbuilder")
 local JSE = CommonSets.JSE;
 
 ---@type LAC.Profile
@@ -66,6 +66,11 @@ sets.MagicAttack = T {
     -- Legs = "Wizard's Tonban",
     Feet = "Rostrum Pumps",
     -- Feet = "Wizard's Sabots",
+};
+
+-- Equipped when we find and equip an elemental staff.
+sets.StaffStrap = T {
+    Sub = "Bugard Strap +1",
 };
 
 sets.ElementalMagic = Utils.compress_tables(sets.MagicAttack, T {
@@ -147,7 +152,7 @@ local CONDITIONAL_GEAR = T {
         return me.MPP < 50 and action.Skill == "Elemental Magic";
     end,
 
-    [T { Main = "Diabolos's Pole" }] = function()
+    [T { Main = "Diabolos's Pole", Sub = "Bugard Strap +1" }] = function()
         local actionName = gData.GetAction().Name;
         if actionName ~= "Drain" and actionName ~= "Aspir" then
             return false;
@@ -261,9 +266,12 @@ profile.HandleMidcast = function()
     end
 
     -- Staff and Obi set.
+    local staff = Shared.getElementalStaff();
+    if staff ~= nil then
+        layers:add(sets.StaffStrap);
+    end
     layers:add(T {
-        Main = Shared.getElementalStaff(),
-        Sub = "Bugard Strap +1",
+        Main = staff,
         Waist = Shared.getElementalObi(),
     });
 
